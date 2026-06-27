bcc9250e4b9581b253bb31812367a48edf05f65f
Aeneas <vasilenkod523@gmail.com>
2026-06-27 15:20:44 +0200

    Add project files.

---
+++ Console Tracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
